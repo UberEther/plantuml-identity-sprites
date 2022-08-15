8151f0a93c2f13c7e6f47015db7d200a8dff4554
Matt Topper <matt@uberether.com>
2022-08-14 22:36:13 -0400

    Dropping in a bunch of junk before I convert it all to grayscale

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# plantuml-identity-sprites
-Build off of Auth0's Identicons this provides a way to easily include identity related images into PlantUML diagrams
+# PlantUML Identity Icon Sprites
+
+## Getting Started
+
+This work was started with examples from the great script by Anthony in the PlantUML repo at [https://github.com/plantuml/plantuml/blob/master/tools/create_sprites.sh](https://github.com/plantuml/plantuml/blob/master/tools/create_sprites.sh)
+
+```puml
+!define ICONURL https://raw.githubusercontent.com/uberether/plantuml-identity-sprites/v1.0.0
+
+Import the sprites that you want
+
+```puml
+!include ../identicons-set-v1/icon-api.puml
+!include ../identicons-set-v2/icon-browser.puml
+!include ../identicons-set-v2/icon-webauthn.puml
+```
+
+or via url
+
+```puml
+!includeurl ICONURL/common.puml
+!includeurl ICONURL/identicons-set-v1/icon-api.puml
+!includeurl ICONURL/identicons-set-v2/icon-browser.puml
+!includeurl ICONURL/identicons-set-v2/icon-webauthn.puml
+```
+
+To use the sprites add one of the macros
+
+```puml
+DEV_MYSQL(db)
+```
+
+The macros are prefixed with the set and the name of the icon
+
+```puml
+<prefix>_<name>(alias)
+<prefix>_<name>(alias,label)
+<prefix>_<name>(alias,label,shape)
+<prefix>_<name>(alias,label,shape,color)
+```
+
+Using the icon from devicons for mysql
+
+```puml
+DEV_MYSQL(db1)
+DEV_MYSQL(db2,label of db2)
+DEV_MYSQL(db3,label of db3,database)
+DEV_MYSQL(db4,label of db4,database,red) #DeepSkyBlue
+```
+
+![overload-example](examples/overload-example.png)
+
+## Icon Sets
+
+The following icon sets are included:
+
+| Name                                                                        | Index                                     |
+| --------------------------------------------------------------------------- | ----------------------------------------- |
+| [Identicons v1](https://identicons.dev/static/icons/identicons-set-v1.zip)  | [List of macros](identicons-set-v1/index.md)   |
+| [Identicons v2](https://github.com/auth0/identicons)                        | [List of macros](identicons-set-v2/index.md) |
+
+## Build
+### Mac OSX
+
+```terminal
+brew install node yarn librsvg openjdk-11-jre graphviz plantuml
+yarn install
+yarn build
+```
+
+## Note
+
+- All brand icons are trademarks of their respective owners.
+- Thanks to tupadr3 for [plantuml-icon-font-sprites](https://github.com/tupadr3/plantuml-icon-font-sprites)
+- Thanks to milo-minderbinder for [AWS-PlantUML](https://github.com/milo-minderbinder/AWS-PlantUML)
+- Thanks to [yuri-becker](https://github.com/yuri-becker) for the integration of [Devicon 2](https://konpa.github.io/devicon/)
+
+## Contributing
+
+Contributions are welcome. In order to update an existing set of icons or to add a new icon set please fork the repository and use a feature branch.
+
+## Changelog
+
+### v1.0.0
+
+- Initial release
+
+Enjoy!
---
+++ create_sprites.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+
+# 
+# (C) Copyright 2017, Anthony Gaudino
+# (C) Copyright 2022, Matt Topper <matt@uberether.com> and UberEther contributors
+# 
+# The following script is derivative work of Anthony's sprite generator script found at:
+# https://github.com/Piotr1215/sprites/blob/master/sprites/create_sprites.sh
+# which is licensed via the GPL. This code therefore is also licensed under the terms
+# of the GNU General Public License.
+# 
+
+#
+# Batch creates sprite files for PlantUml
+#
+# Given a directory can convert all PNG files to PlantUml sprite files.
+#
+# The generated sprites files formats are based on the ones introduced in
+# PlantUml 1.2017.19.
+#
+# This script assumes plantuml is in PATH.
+#
+
+
+# Help usage message
+usage="Batch creates sprite files for PlantUml.
+
+$(basename "$0") [options] prefix
+
+options:
+    -p  directory path to process  Default: ./
+    -w  width  of PNG              Default: 48
+    -h  height of PNG              Default: 48
+    -g  sprite graylevel           Default: 16
+    
+    prefix: a prefix that is added to the sprite name"
+
+
+
+# Default arguments values
+        dir="./"  # Directory path to process     Default: ./
+      width=48    # Width  of PNG to generate     Default: 48
+     height=48    # Height of PNG to generate     Default: 48
+  graylevel=16    # Number of grayscale colors    Default: 16
+
+     prefix=""    # Prefix for sprites names, avoids having
+prefixupper=""    # two sprites with same name on STDLIB
+
+
+
+########################################
+#
+#    Main function
+#
+########################################
+main () {
+    # Get arguments
+    while getopts p:w:h:g:s option
+    do
+        case "$option" in
+            p)        dir="$OPTARG";;
+            w)      width="$OPTARG";;
+            h)     height="$OPTARG";;
+            g)  graylevel="$OPTARG";;
+            :) echo "$usage"
+               exit 1
+               ;;
+           \?) echo "$usage"
+               exit 1
+               ;;
+        esac
+    done
+
+    # Get mandatory argument
+    shift $(($OPTIND-1))
+    prefix=$(     echo $1 | tr '[:upper:]' '[:lower:]')
+    prefixupper=$(echo $1 | tr '[:lower:]' '[:upper:]')
+
+    # Check mandatory argument
+    if [ -z "$prefix" ]
+    then
+        echo "Please specify a prefix!"
+        echo "$usage"
+        exit 1
+    fi
+
+
+
+    # Change dir to where images are
+    if [ ! -d "${dir}" ]
+    then
+        echo "Please specify a valid directory"
+        echo "$usage"
+        exit 1
+    fi
+    
+    cd "$dir"
+
+    process_png
+}
+
+
+########################################
+#
+#    Generate PlantUml sprite
+#
+########################################
+process_png () {
+    for i in *.png
+    do
+        [ -f "$i" ] || continue
+
+            filename=$(echo $i | sed -e 's/.png$//')                        # Filename without extension
+            filenameupper=$(echo $filename | tr '[:lower:]' '[:upper:]')    # Filename without extension in uppercase
+            spritename="${prefix}_$filename"                                # Sprite name is composed by prefix_filename
+            spritenameupper="${prefixupper}_$filenameupper"                 # Sprite name in uppercase
+            spritestereo="$prefixupper $filenameupper"                      # Sprite stereotype is uppercase prefix followed by uppercase filename
+            stereowhites=$(echo $spritestereo | sed -e 's/./ /g')           # This is just whitespace to make output nicer
+
+        #echo "@startuml" > $filename.puml
+
+        echo -e "$(plantuml -encodesprite $graylevel $i | sed -e '1!b' -e 's/\$/$'${prefix}_'/')\n" > $filename.puml
+
+        echo "!define $spritenameupper(_color)                                 SPRITE_PUT(          $stereowhites          $spritename, _color)"                 >> $filename.puml
+        echo "!define $spritenameupper(_color, _scale)                         SPRITE_PUT(          $stereowhites          $spritename, _color, _scale)"         >> $filename.puml
+
+        echo "!define $spritenameupper(_color, _scale, _alias)                 SPRITE_ENT(  _alias, $spritestereo,         $spritename, _color, _scale)"         >> $filename.puml
+        echo "!define $spritenameupper(_color, _scale, _alias, _shape)         SPRITE_ENT(  _alias, $spritestereo,         $spritename, _color, _scale, _shape)" >> $filename.puml
+        echo "!define $spritenameupper(_color, _scale, _alias, _shape, _label) SPRITE_ENT_L(_alias, $spritestereo, _label, $spritename, _color, _scale, _shape)" >> $filename.puml
+        
+        echo "skinparam folderBackgroundColor<<$prefixupper $filenameupper>> White"                                                                              >> $filename.puml
+        
+        #echo "@enduml" >> $filename.puml
+    done
+}
+
+
+main "$@"
